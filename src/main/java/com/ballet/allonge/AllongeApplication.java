2b1b550eec28df499130f3597eb93a48db9b4aca
Duyan Kim <arbolkim@gmail.com>
2023-07-17 23:50:24 +0900

    chore: intial commit

---
+++ src/main/java/com/ballet/allonge/AllongeApplication.java
@@ -0,0 +1,13 @@
+package com.ballet.allonge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllongeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllongeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ballet/allonge/AllongeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ballet.allonge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllongeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
